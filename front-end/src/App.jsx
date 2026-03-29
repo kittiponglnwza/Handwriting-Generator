784c051215017da37dde866e5dde3c72aae0d706
T.KITTIPONG <kitipongzaza566@gmail.com>
2026-03-29 17:20:49 +0700

    start

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-6xl font-bold text-green-400">
+        Tailwind OK 🔥
+      </h1>
+    </div>
+  )
+}
+
+export default App
